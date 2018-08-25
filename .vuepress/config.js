4f40d1cbb2ae2c4ef534621f8b89c94dd51c83a1
yiibryan <yiibryan@hotmail.com>
2018-08-25 18:18:20 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,111 @@
+module.exports = {
+  title: '前端代码规范',
+  description: '为了增强团队开发协作、提高代码质量和提升编码速度的规范。',
+  head: [
+    ['link', { rel: 'icon', href: `/icons/favicon-32x32.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  serviceWorker: true,
+  themeConfig: {
+    // search: false,
+    logo: '/icons/apple-touch-icon-152x152.png',
+    searchMaxSuggestions: 10,
+    lastUpdated: '上次更新',
+    serviceWorker: {
+      updatePopup: {
+        message: "发现新内容可用",
+        buttonText: "刷新"
+      }
+    },
+    nav: [
+      {
+        text: '代码规范',
+        link: '/guide/',
+      },
+      {
+        text: 'markdown手册',
+        link: '/markdown/'
+      },
+      {
+        text: '更新日志',
+        link: 'https://github.com/yiibryan/front-end-docs/blob/master/CHANGELOG.md'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: '开始使用',
+          collapsable: false,
+          children: [
+            '',
+          ]
+        },
+        {
+          title: 'HTML 规范',
+          collapsable: false,
+          children: [
+            'html/code',
+            'html/note',
+            'html/template',
+            'html/webapp'
+          ]
+        },
+        {
+          title: 'CSS 规范',
+          collapsable: false,
+          children: [
+            'css/code',
+            'css/note',
+            'css/sass',
+            'css/reset',
+            'css/query',
+            'css/webkit'
+          ]
+        },
+        {
+          title: 'javascript 规范',
+          collapsable: false,
+          children: [
+            'javascript/code',
+            'javascript/language'
+          ]
+        },
+        {
+          title: '图片规范',
+          collapsable: false,
+          children: [
+            'image/format',
+            'image/import',
+            'image/quality',
+            'image/size'
+          ]
+        },
+        {
+          title: '命名规范',
+          collapsable: false,
+          children: [
+            'name/dir',
+            'name/image',
+            'name/htmlcss',
+            'name/classname'
+          ]
+        }
+      ],
+      '/markdown/': [
+        {
+          collapsable: false,
+          children: [
+            ''
+          ]
+        }
+      ]
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+---
+home: true
+heroImage: /logo.png
+actionText: 快速开始 →
+actionLink: /guide/
+features:
+- title: HTML规范
+  details: 以 W3C 为中心，让页面HTML代码更具语义性。
+- title: CSS规范
+  details: 统一团队 CSS 代码书写风格、命名规范，使用 CSS 预编译语言语法风格，提供常用媒体查询语句和浏览器私有属性引用，并从业务层面统一规范常用模块的引用。
+- title: JavaScript规范
+  details: 统一团队的 JS 语法风格、书写习惯、命名规范，减少程序出错的概率，其中也包含了  ES6 的语法规范和最佳实践。
+- title: 图片规范
+  details: 了解各种图片格式特性，根据特性制定图片规范，包括但不限于图片的质量约定、图片引入方式、图片合并处理等。
+- title: 命名规范
+  details: 从 "目录命名"、"图片命名"、"类命名" 等层面约定规范团队的命名习惯，增强团队代码的可读性。
+- title: 
+  details:   
+footer: MIT Licensed | Copyright © 2018-present
+---
